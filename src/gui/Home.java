1ea98b316a680227b0a749ed117ea4be87ca090b
Find <findxiaoxun@gmail.com>
2014-04-02 11:48:33 +0800

    version 04 the preshow

---
+++ src/gui/Home.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.JFrame;
+
+/*The home of the project*/
+public class Home extends JFrame{
+	private static final long serialVersionUID = -4731380524981415485L;
+	public Home() {
+		
+	}
+}
--- src/test.java
+++ src/test.java
@@ -1,17 +1,56 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
 
+import gui.PreShow;
 
-public class test {
-	public static void main(String[] args) {
-		try {
-			Scanner scan=new Scanner(new File("res/da.txt"));
-			System.out.println(scan.nextLine());
-			scan.close();
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-}
+import javax.swing.JFrame;  
+import javax.swing.UIManager;  
+  
+/** 
+ * 
+ * @author 杨胜寒 
+ */  
+public class test {  
+  
+    public static void main(String args[]) {  
+        try {  
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());  
+        } catch (Exception e) {  
+        }  
+        //初始化闪屏Dialog时指定闪屏图片  
+        final PreShow splashWindow = new PreShow();  
+        //启动一个线程来加载数据  
+        new Thread() {  
+            @Override  
+            public void run() {  
+                try {  
+                    for (int i = 0; i < 10; i++) {  
+                        splashWindow.updateProcess("正在进行第" + i + "次缓存数据加载. . .", i * 9);  
+                        Thread.sleep(300);  
+                    }  
+                } catch (InterruptedException ex) {  
+                    //异常不做处理  
+                }  
+                JFrame window = new JFrame();  
+                splashWindow.updateProcess("正在启动主窗体. . .", 100);  
+                moveToScreenCenter(window);  
+                splashWindow.setVisible(false);  
+                //数据加载完成，显示主窗体  
+                window.setVisible(true);  
+                //释放资源  
+                splashWindow.dispose();  
+            }  
+        }.start();  
+        //显示闪屏Dialog  
+        splashWindow.setVisible(true);  
+    }  
+    public static final void moveToScreenCenter(Component comp) {   
+        Dimension screenSize=Toolkit.getDefaultToolkit().getScreenSize();
+        int centerX = screenSize.width/2;
+        int centerY = screenSize.height/2;
+        Point point = new Point(centerX, centerY);
+        comp.setLocation(point);
+      }
+}  
